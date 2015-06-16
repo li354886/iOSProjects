a08cabeea75ed41ba2c4416ad9c435647fac4fac
Zhengning Li <zl132@georgetown.edu>
2015-06-15 23:17:57 -0400

    fixed some bugs

--- Caculator/Caculator/CaculatorBrain.swift
+++ Caculator/Caculator/CaculatorBrain.swift
@@ -28,8 +28,35 @@ class CaculatorBrain
         knowOps["√"] = Op.UnaryOperation("√") { sqrt($0)}
     }
     
-    func evalute() -> Double? {
-        
+    private func evaluate(let ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps);
+            case .UnaryOperation(_, let operation):
+                let operandEvalutaion = evaluate(remainingOps)
+                if let operand = operandEvalutaion.result {
+                    return (operation(operand), operandEvalutaion.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return(nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, _) = evaluate(opStack)
+        return result
     }
     
     func pushOperand(operand: Double) {
